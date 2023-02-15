fdcd8b7ec25cb619aaf4af5b56e7a2dae53c5acf
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2023-02-15 01:10:51 +0100

    Skeleton of levenshtein distance

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
 use std::io;
+use core::cmp;
 
-fn calc_edit_distance(str1: &str, str2: &str) -> u32 {
-    todo!()
+fn min(a: usize, b: usize, c: usize) -> usize {
+    cmp::min(a, cmp::min(b, c))
+}
+
+fn calc_edit_distance(str1: &str, str2: &str) -> usize {
+    let m = str1.len() + 1;
+    let n = str2.len() + 1;
+    let mut matrix = vec![vec![0; m]; n];
+
+    // Prefixes of str1 can be transformed into the empty string (0th prefix of str2)
+    // By removing all characters
+    for i in 1..m {
+        matrix[0][i] = i;
+    }
+    
+    // Empty str1 can be transformed into the prefixes by adding every character
+    for i in 1..n {
+        matrix[i][0] = i;
+    }
+
+    for i in 1..n {
+        for j in 1..m {
+            let mut cost = 0;
+            if str1.as_bytes()[j] != str2.as_bytes()[i] {
+                cost = 1;
+            }
+
+            matrix[j][i] = min(i, j, j + 1); // Test
+        }
+    }
+
+    return matrix[n - 1][m - 1];
 }
 
 fn main() {
@@ -24,7 +55,7 @@ fn main() {
         .map(|x| x.unwrap());
 
     for misspelled_word in lines {
-        let mut shortest_distance = u32::MAX;
+        let mut shortest_distance = usize::MAX;
         let mut matching_words: Vec<String> = vec![];
 
         // Calculate edit distance between misspelled word and every word in the word list
